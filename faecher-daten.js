7c9621d6128471f4ec39ae6c2916b82058e7f41e
Mary-cyber-code <olga.voelzke@proton.me>
2026-07-12 17:28:30 +0200

    Add subject data for various school subjects

---
+++ faecher-daten.js
@@ -0,0 +1,32 @@
+const subjectData = {
+    mathe: {
+        title: "Mathematik",
+        categories: {
+            "Analysis (K2)": [
+                { name: "Ableitungen & Stammfunktionen (Druck-Version)", path: "lernzettel/mathe/analysis_print.html" },
+                { name: "Ableitungen (Interaktive Online-Version)", path: "lernzettel/mathe/analysis_interaktiv.html" }
+            ],
+            "Geometrie (K1)": [
+                { name: "Vektoren Grundlagen", path: "lernzettel/mathe/vektoren.html" }
+            ]
+        }
+    },
+    chemie: {
+        title: "Chemie",
+        categories: {
+            "Organische Chemie": [
+                { name: "Alkanole und Ester", path: "lernzettel/chemie/alkanole.html" }
+            ]
+        }
+    },
+    gmk: { title: "Gemeinschaftskunde", categories: {} },
+    deutsch: { title: "Deutsch", categories: {} },
+    englisch: { title: "Englisch", categories: {} },
+    bio: { title: "Biologie", categories: {} },
+    kunst: { title: "Kunst", categories: {} },
+    sport: { title: "Sport", categories: {} },
+    religion: { title: "Religion", categories: {} },
+    geschichte: { title: "Geschichte", categories: {} },
+    geo: { title: "Geographie", categories: {} },
+    psy: { title: "Psychologie", categories: {} }
+};
