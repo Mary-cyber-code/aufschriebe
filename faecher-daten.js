1c4ca756429fbf005266b1d10a853574df3ba163
Mary-cyber-code <olga.voelzke@proton.me>
2026-07-12 17:59:35 +0200

    Add categories to Chemie subject data

--- faecher-daten.js
+++ faecher-daten.js
@@ -1,6 +1,8 @@
 const subjectData = {
     mathe: { title: "Mathematik", categories: {} },
-    chemie: { title: "Chemie", categories: {} },
+    chemie: { title: "Chemie", categories: {
+        lernzettel/chemie/Struktur- und Eigenschaftsbeziehungen.html
+    } },
     gmk: { title: "Gemeinschaftskunde", categories: {} },
     deutsch: { title: "Deutsch", categories: {} },
     englisch: { title: "Englisch", categories: {} },
